c1a09a8132301d403137a18eb54c0538a2e0ecd8
Lbringer <g.aditya2307@gmail.com>
2022-03-15 08:38:54 +0530

    disabled scrolling when pop-up menu is there

--- ResponsiveWeb/InteriorConsultant/index.js
+++ ResponsiveWeb/InteriorConsultant/index.js
@@ -2,10 +2,25 @@
 const menuIcon = document.querySelector("#menu-icon");
 const menu = document.querySelector("#menu");
 const close = document.querySelector("#close");
+const html = document.querySelector("html");
+const body = document.querySelector("body");
 
 menuIcon.addEventListener('click', () => {
     menu.style.display = "flex";
+    html.style.margin = "0";
+    html.style.height = "100%";
+    html.style.overflow = "hidden";
+    body.style.margin = "0";
+    body.style.height = "100%";
+    body.style.overflow = "hidden";
+
 })
 close.addEventListener('click', () => {
     menu.style.display = "none";
+    html.style.height = "fit-content";
+    html.style.overflow = "visible";
+    body.style.margin = "none";
+    body.style.height = "fit-content";
+    body.style.overflow = "visible";
 })
+// html, body { margin: 0; height: 100 %; overflow: hidden }
